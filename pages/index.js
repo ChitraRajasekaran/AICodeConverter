004c3160373cbbc71bffa7af148e171e498cca6f
Chitra Rajasekaran <crinspire01@gmail.com>
2024-06-20 15:24:56 -0400

    updated with minor fragment changes

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,12 @@
 import Header from "@/components/header";
-const { Fragment } = require("react");
 import Apikey from "@/components/apikey";
 
 function aicodeconvertor() {
     return (
-        <Fragment>
             <div className="flex flex-col items-center justify-center h-screen">
                 <Header />
                 <Apikey />
             </div>
-        </Fragment>
     )
 }
 
